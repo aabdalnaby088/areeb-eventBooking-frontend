f6b1e8568a85cfa6f1903d2e33cdbcff2948b028
aabdalnaby088 <11410120210035@stud.cu.edu.eg>
2025-05-17 02:23:52 +0300

    Final commit insallah

--- src/App.tsx
+++ src/App.tsx
@@ -1,44 +1,49 @@
-import {  HashRouter, Route, Routes } from "react-router-dom";
-import Layout from "./Layout";
-import Notfound from "./pages/Not-found";
-import Home from "./pages/Home";
-import "./index.css";
-import Signup from "./pages/Signup";
-import { Provider } from "react-redux";
-import { store } from "./redux/store";
-import Login from "./pages/Login";
-import { ReactQueryProvider } from "./lib/QueryClientProvider";
-import Events from "./pages/Events";
-import EventDetails from "./pages/EventDetails";
-import Cart from "./pages/Cart";
-import Congratulations from "./components/Congratulations";
+import { HashRouter, Routes, Route } from 'react-router-dom';
+import { Provider } from 'react-redux';
 import { Toaster } from 'react-hot-toast';
-import Admin from "./pages/Admin";
+import Layout from './Layout'
+import Home from './pages/Home';
+import Cart from './pages/Cart';
+import EventDetails from './pages/EventDetails';
+import Events from './pages/Events';
+import Notfound from './pages/Not-found';
+import Congratulations from './pages/Congratulations';
+import Admin from './pages/Admin';
+import Signup from './pages/Signup';
+import Login from './pages/Login';
+import {ReactQueryProvider} from './lib/QueryClientProvider';
+import ProtectedRoute from './components/ProtectedRoutes';
+import {store} from './redux/store';
 
 function App() {
   return (
     <HashRouter>
-    <ReactQueryProvider>
-      <Toaster position="top-center" reverseOrder={false} />
-    <Provider store={store}>
-
-    <Routes>
-      <Route path="/" element={<Layout />}>
-        <Route index element={<Home/>} />
-
-        <Route path="cart" element={<Cart/>} />
-        <Route path="event/:id" element={<EventDetails/>} />
-        <Route path="events" element={<Events/>} />
-        <Route path="events/:category" element={<Events/>} />
-        <Route path="*" element={<Notfound/>} />
-        <Route path="congratulations" element={<Congratulations/>} />
-      </Route>
-        <Route path="Admin" element={<Admin/>} />
-      <Route path="/Signup" element={<Signup/>} />
-      <Route path="login" element={ <Login/> } />
-    </Routes>
-    </Provider>
-    </ReactQueryProvider>
+      <ReactQueryProvider>
+        <Toaster position="top-center" reverseOrder={false} />
+        <Provider store={store}>
+          <Routes>
+            <Route path="/" element={<Layout />}>
+              <Route index element={<Home />} />
+              <Route path="cart" element={<Cart />} />
+              <Route path="event/:id" element={<EventDetails />} />
+              <Route path="events" element={<Events />} />
+              <Route path="events/:category" element={<Events />} />
+              <Route path="*" element={<Notfound />} />
+              <Route path="congratulations" element={<Congratulations />} />
+            </Route>
+            <Route
+              path="Admin"
+              element={
+                <ProtectedRoute>
+                  <Admin />
+                </ProtectedRoute>
+              }
+            />
+            <Route path="/Signup" element={<Signup />} />
+            <Route path="login" element={<Login />} />
+          </Routes>
+        </Provider>
+      </ReactQueryProvider>
     </HashRouter>
   );
 }
